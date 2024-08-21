d743238631a4eec8e387c5b9e672dfda48a8e95b
Arthur <84734090+rapach01@users.noreply.github.com>
2024-08-21 18:52:50 -0300

    Add files via upload

---
+++ pages/home.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bete Arteira</title>
+    <link rel="icon" href="../assets/img/logo_b.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../assets/css/home.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg" style="background-color: #73BBCA">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">
+                <img src="../assets/img/logo_b.png" alt="" width="50" height="34">
+                Bete Arteira
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">Sobre mim</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#services">Serviços</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- banner -->
+    <section>
+        <div class="banner_home">
+            <img src="../assets/img/banner_home_certo.png" class="img-fluid" alt="Banner Home">
+        </div>
+    </section>
+    <!-- sobre mim -->
+    <section id="about" style="background-color: #73BBCA;">
+    <div class="container">
+        <div class="row p-5">
+            <div class="col-12">
+                <h1>Que tal conhecer um pouco sobre minha história e como vim parar no mundo dos jogos pedagógicos?</h1>
+                <br>
+                <p><strong>Elizabete</strong>, 53 anos, mãe do Arthur, Eliana e Natália, avó do Raphael.</p>
+                <p>Formada em Artes Visuais desde 2017 e atuante na área em escolas desde 2018. Formada em Séries Iniciais com diversos cursos na área da Educação infantil, fundamental e médio. Atualmente atuando como professora de Artes Visuais, e também cursando psicopedagogia.</p>
+                <p>Os jogos pedagógicos entraram na minha caminhada já na primeira formação. Desde então, a criatividade é rotineira em criação de jogos por aqui. Criando jogos para colegas da área utilizarem, mães e amigas próximas.</p>
+                <p>A ideia de replicar essa didática para mais pessoas surgiu quando minha filha, graduanda em Fonoaudiologia, pediu para que replicasse alguns jogos para aplicação de terapias e pensei, por que não ajudar mais profissionais da área?</p>
+                <p>Buscando conhecimento aprofundado na área da psicopedagogia, e com diversos certificados de cursos em jogos pedagógicos, aqui estou! Pronta pra te auxiliar nas terapias aplicadas.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- serviços -->
+    <section id="services" style="background-color: #E887AA">
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Conheça os serviços que ofereço</h1>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/numeros.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/lego.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/logo.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/cavalinhos.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/montagem.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4 col-md-6 col-sm-6 p-2">
+                    <div class="card">
+                        <img src="../assets/img/enfeites.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Jogos Pedagógicos</h5>
+                            <p class="card-text">Jogos pedagógicos para terapias aplicadas.</p>
+                            <a href="#" class="btn btn-primary">Saiba mais</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contato -->
+    <section id="contact" style="background-color: #4F85AF">
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Entre em contato</h1>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-12 ">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nome</label>
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">E-mail</label>
+                            <input type="email" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Assunto</label>
+                            <input type="text" class="form-control" id="exampleInputPassword1">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Mensagem</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Enviar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer style="background-color: #73BBCA">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <p class="footer-text">Desenvolvido por <a href="#">Arthur</a></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
